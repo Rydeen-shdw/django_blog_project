9e94353b9721ee8f4a2e6107436fc6d2d6864558
ryd33n <shdw.rydeen.dev@gmail.com>
2023-11-15 17:31:07 +0200

    Add post list

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from blog import views
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    path('', views.post_list_view, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog import models
+
+
+def post_list_view(request):
+    posts = models.Post.objects.all()
+    return render(request, 'blog/post/post_list.html', {'posts': posts})
