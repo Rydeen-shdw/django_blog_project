71bf4b3b33848efbce812b4363542bdefc8a0747
ryd33n <shdw.rydeen.dev@gmail.com>
2023-11-24 00:17:25 +0200

    Fix: weather data parsing

--- weather/services.py
+++ weather/services.py
@@ -1,13 +1,24 @@
-
 import requests
 from datetime import datetime
 
 from django.conf import settings
 
 
-def get_weather_data(city):
-    connect_timeout = 5
+def parse_weather_data(data):
+    data = {
+        'temp': data["main"]["temp"],
+        'icon': data["weather"][0]["icon"],
+        'weather': data["weather"][0]["description"],
+        'wind_speed': data["wind"]["speed"] * 3.6,
+        'humidity': data["main"]["humidity"],
+        'time': data["timezone"],
+        'sunrise': datetime.utcfromtimestamp(data["sys"]["sunrise"]).strftime('%H')
+    }
+    return data
 
+
+def get_weather_data(city):
+    connect_timeout = 10
     key = settings.OPENWEATHERMAP_API_KEY
     url = 'https://api.openweathermap.org/data/2.5/weather'
     params = {
@@ -15,27 +26,15 @@ def get_weather_data(city):
         'units': 'metric',
         'appid': key,
     }
-    response = requests.get(url, params=params, timeout=connect_timeout )
+    response = requests.get(url, params=params, timeout=connect_timeout)
     response.raise_for_status()
-    data = response.json()
     if response.status_code == 200:
-        return data
+        data = response.json()
+        parsed_data = parse_weather_data(data)
+        return parsed_data
     else:
         return None
 
-def get_weather(city):
-    data = get_weather_data(city)
-    if data:
-        data = {
-            'temp': data["main"]["temp"],
-            'icon': data["weather"][0]["icon"],
-            'weather': data["weather"][0]["description"],
-            'wind_speed': data["wind"]["speed"] * 3.6,
-            'humidity': data["main"]["humidity"],
-            'time': data["timezone"],
-            'sunrise': datetime.utcfromtimestamp(data["sys"]["sunrise"]).strftime('%H')
-        }
-        return data
-    return None
+
 
 
