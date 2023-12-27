df0f6112a759c1c92a87a29f975bf53c47aab3e2
ryd33n <shdw.rydeen.dev@gmail.com>
2023-12-27 16:38:44 +0200

    Add: Posts api urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views import movies_views, accounts_views
+from api.views import movies_views, accounts_views, posts_views
 
 app_name = 'api'
 
@@ -9,5 +9,7 @@ urlpatterns = [
     # movies
     path('movies/movies/', movies_views.MovieListAPIView.as_view(), name='movies_list'),
     path('accounts/create/', accounts_views.UserCreateAPIView.as_view(), name='register'),
-    path('accounts/activate/', accounts_views.ActivateUserAPIView.as_view(),name='activate')
+    path('accounts/activate/', accounts_views.ActivateUserAPIView.as_view(),name='activate'),
+    path('posts/posts/', posts_views.PostsListAPIView.as_view(), name='posts_list'),
+    path('posts/posts/<str:slug>/', posts_views.PostAPIView.as_view(), name='posts')
 ]
